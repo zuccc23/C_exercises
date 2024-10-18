7d6ba6703e7ca741b814c25aba06f25777ab0c88
zuccc23 <daniyaahmane@gmail.com>
2024-10-18 17:44:48 +0200

    commented linked list

--- clist.c
+++ clist.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node Node ;
+typedef struct Node Node ; //create alias
 
-struct Node {
-    int data;
-    Node *next;
+struct Node { //create structure
+    int data; //variable containing the data
+    Node *next; //pointer to the next node
 };
 
-Node *createNode(int data) {
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
+Node *createNode(int data) { //function to create new node 
+    Node *newNode = (Node*)malloc(sizeof(Node)); //allocate memory
+    //you need to convert the 'void *' that malloc returns to type 'Node *'
+    newNode->data = data;//assigns the data entered as parameter
+    newNode->next = NULL; //assigns next node to NULL (useful if its the last one)
     return (newNode);
 }
 
-void nHead(Node **head, int data){
-    Node *newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
+void nHead(Node **head, int data){ //function to create new nodes one before the other
+    Node *newNode = createNode(data);//use function to create new node and put it as head
+    newNode->next = *head;//old head assigned to the next node 
+    *head = newNode;//old head updated to new node 
 }
 
-void printList(Node *head){
-    Node *temp = head;
+void printList(Node *head){//function to print the list
+    Node *temp = head;//create temporary variable to hold the head
     while (temp != NULL){
-        printf("%d\n", temp->data);
-        temp = temp->next;
+        printf("%d\n", temp->data);//print the data
+        temp = temp->next;//assign temp to next node in order to move in the list
     }
 }
 
 int main(void) {
     int i = 1;
     Node *head = NULL;
-    while (i <= 5){
+    while (i <= 5){//loop to create new nodes
         nHead(&head, i);
         i++;
     }
-    //nHead(&head, 5);
-    //nHead(&head, 4);
-
     printList(head);
-    //printf("%d\n", head->data);
 }
