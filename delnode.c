7f2652b2af150afdebdb4bbc9cbce19e4fe00b9f
zuccc23 <daniyaahmane@gmail.com>
2024-10-19 15:54:46 +0200

    delete node

---
+++ delnode.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node Node;
+
+struct Node {
+    int data;
+    Node *next;
+};
+
+Node *createNode(int data) {
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL)
+        return (NULL);
+    newNode->data = data;
+    newNode->next = NULL;
+    return (newNode);
+}
+
+void endlist(Node **head, int data){
+    Node *newNode = createNode(data);
+    Node *temp = *head;
+    if (*head == NULL){
+        *head = newNode;
+        return;
+    }
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void delnode(Node **head, int i){
+    Node *temp = *head;
+    Node *temp2 = *head;
+    while (temp2->data != i){
+        temp2 = temp2->next;
+    }
+    while (temp->data != i-1){
+        temp = temp->next;
+    }
+    temp->next = temp2->next;
+    //temp = temp->next;
+}
+
+void printlist(Node *head){
+    Node *temp = head;
+    while (temp != NULL){
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main(void){
+    Node *head = NULL;
+    int i = 1;
+    while (i < 11){
+        endlist(&head, i);
+        i++;
+    }
+    printf("first list : \n");
+    printlist(head);
+    delnode(&head, 7);
+    printf("second list :\n");
+    printlist(head);
+}
