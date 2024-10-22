e6523da03ef53cd0bb2f0e9db3e88545951e31f7
zuccc23 <daniyaahmane@gmail.com>
2024-10-22 12:49:21 +0200

    ft any

---
+++ ft_any.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(char *str){
+    int i = 0;
+    while (str[i]){
+        if (str[i] >= 'A' && str[i] <= 'Z'){
+            return (1);
+        }
+        i++;
+    }
+    return (0);
+}
+
+int ft_any(char **tab, int(*f)(char *)){
+    int i = 0;
+    while (tab[i] != NULL){
+        if (f(tab[i]) == 1)
+            return (1);
+        i++;
+    }
+    return(0);
+}
+
+// char *str = "Daniya";
+// char *strr = "Mario";
+// char *strs[] = {"Daniya", "Mario", NULL};
+
+int main(void){
+    char *tab[]= {"bbbbe", "gyhdjzeHvdhc", NULL};
+    printf("%d\n", ft_any(tab, &f));
+}
