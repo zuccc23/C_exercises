1f519dd599fe42b7a5619f1cfefb125a1a9b4369
zuccc23 <daniyaahmane@gmail.com>
2024-10-28 14:34:09 +0100

    memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,41 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/27 16:21:47 by dahmane           #+#    #+#             */
-/*   Updated: 2024/10/28 14:33:36 by dahmane          ###   ########.fr       */
+/*   Created: 2024/10/28 14:16:34 by dahmane           #+#    #+#             */
+/*   Updated: 2024/10/28 14:30:10 by dahmane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*sz;
-	
-	sz = (unsigned char *)s;
-	while (n > 0)
+	unsigned char *sz1;
+	unsigned char *sz2;
+
+	sz1 = (unsigned char *)s1;
+	sz2 = (unsigned char *)s2;
+	if (n <= 0)
+		return (0);
+	while (*sz1 && n - 1 > 0)
 	{
-		if (*sz == (unsigned char*)c)
-			return ((void*)sz);
-		sz++;
+		if (*sz1 != *sz2)
+			return (*sz1 - *sz2);
+		sz1++;
+		sz2++;
 		n--;
 	}
-	return (NULL);
+	return (*sz1 - *sz2);
 }
 
 int	main(void)
 {
 	int c = 'e';
 	size_t n = 1;
-	char str[] = "hello";
+	char str[] = "iello";
 	char strr[] = "hello";
-	char *res = memchr(str, c, n);
 	//memchr(str, c, n);
-	ft_memchr(strr, c, n);
-	printf("%s\n", ft_memchr(strr, c, n));
+	//ft_memchr(strr, c, n);
+	printf("%d\n", ft_memcmp(str, strr, n));
 	//printf("%s\n", ft_memchr(strr, c));
 }
